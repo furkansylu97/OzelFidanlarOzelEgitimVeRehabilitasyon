cbb320f603f5bc548b44df35564175b50c9cdee9
Furkan Soylu <furkansylu97@gmail.com>
2026-02-06 02:40:57 +0300

    feat(special-learning-symptoms-page): add responsive design and layout optimizations

--- OzelFidanlar/src/features/home/pages/special-learning-disability-symptoms-page/SpecialLearningDisabilitySymptomsPage.tsx
+++ OzelFidanlar/src/features/home/pages/special-learning-disability-symptoms-page/SpecialLearningDisabilitySymptomsPage.tsx
@@ -4,7 +4,7 @@ const SpecialLearningDisabilitySymptomsPage = () => {
   return (
     <>
       <SpecialLearningDisabilitySymptomsSection />
-      <section aria-hidden className="h-24" />
+      <section aria-hidden className="h-10 lg:h-24" />
     </>
   );
 };
