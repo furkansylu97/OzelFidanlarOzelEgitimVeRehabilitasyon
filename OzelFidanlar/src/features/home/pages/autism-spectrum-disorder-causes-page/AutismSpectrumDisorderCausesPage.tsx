758d6b4139775047993bbe1b93de4b0016c04997
Furkan Soylu <furkansylu97@gmail.com>
2026-02-07 02:02:40 +0300

    feat(autism-spectrum-disorder-causes-page): add responsive design and layout optimizations

--- OzelFidanlar/src/features/home/pages/autism-spectrum-disorder-causes-page/AutismSpectrumDisorderCausesPage.tsx
+++ OzelFidanlar/src/features/home/pages/autism-spectrum-disorder-causes-page/AutismSpectrumDisorderCausesPage.tsx
@@ -4,7 +4,7 @@ const AutismSpectrumDisorderCausesPage = () => {
   return (
     <>
       <AutismSpectrumDisorderCausesSection />
-      <section aria-hidden className="h-24" />
+      <section aria-hidden className="h-10 lg:h-24" />
     </>
   );
 };
