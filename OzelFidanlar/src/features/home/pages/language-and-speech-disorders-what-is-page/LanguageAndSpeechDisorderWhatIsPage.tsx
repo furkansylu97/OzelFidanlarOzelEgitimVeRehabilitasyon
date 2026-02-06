4d53695d4a42b5589f3dfdaa08aab9059fe80f4b
Furkan Soylu <furkansylu97@gmail.com>
2026-02-06 17:07:34 +0300

    feat(speech-and-language-disorders-what-is-page): add responsive design and layout optimizations

--- OzelFidanlar/src/features/home/pages/language-and-speech-disorders-what-is-page/LanguageAndSpeechDisorderWhatIsPage.tsx
+++ OzelFidanlar/src/features/home/pages/language-and-speech-disorders-what-is-page/LanguageAndSpeechDisorderWhatIsPage.tsx
@@ -4,7 +4,7 @@ const LanguageAndSpeechDisorderWhatIsPage = () => {
   return (
     <>
       <LanguageAndSpeechDisorderWhatIsSection />
-      <section aria-hidden className="h-24" />
+      <section aria-hidden className="h-10 lg:h-24" />
     </>
   );
 };
