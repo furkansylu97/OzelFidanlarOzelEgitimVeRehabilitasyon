c8dff175c065256fb03e561cb341478089198fdf
Furkan Soylu <furkansylu97@gmail.com>
2026-02-07 01:39:05 +0300

    feat(autism-spectrum-disorder-what-is-page): add responsive design and layout optimizations

--- OzelFidanlar/src/features/home/pages/autism-spectrum-disorder-what-is-page/AutismSpectrumDisorderWhatIsPage.tsx
+++ OzelFidanlar/src/features/home/pages/autism-spectrum-disorder-what-is-page/AutismSpectrumDisorderWhatIsPage.tsx
@@ -4,7 +4,7 @@ const AutismSpectrumDisorderWhatIsPage = () => {
   return (
     <>
       <AutismSpectrumDisorderWhatIsSection />
-      <section aria-hidden className="h-24" />
+      <section aria-hidden className="h-10 lg:h-24" />
     </>
   );
 };
