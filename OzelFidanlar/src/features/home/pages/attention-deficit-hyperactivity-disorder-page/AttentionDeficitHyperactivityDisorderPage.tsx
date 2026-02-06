9472ffc75f8fcd3fc7c0e58ddbb20cb533838d02
Furkan Soylu <furkansylu97@gmail.com>
2026-02-07 02:13:43 +0300

    feat(attention-deficit-hyperactivity-disorder-page): add responsive design and layout optimizations

--- OzelFidanlar/src/features/home/pages/attention-deficit-hyperactivity-disorder-page/AttentionDeficitHyperactivityDisorderPage.tsx
+++ OzelFidanlar/src/features/home/pages/attention-deficit-hyperactivity-disorder-page/AttentionDeficitHyperactivityDisorderPage.tsx
@@ -4,7 +4,7 @@ const AttentionDeficitHyperactivityDisorderPage = () => {
   return (
     <>
       <AttentionDeficitHyperactivityDisorderSection />
-      <section aria-hidden className="h-24" />
+      <section aria-hidden className="h-10 lg:h-24" />
     </>
   );
 };
