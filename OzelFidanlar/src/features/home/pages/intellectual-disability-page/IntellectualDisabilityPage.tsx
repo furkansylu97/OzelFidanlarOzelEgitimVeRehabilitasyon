67d427768b28e3471ca5927517b0ff85d2cb861d
Furkan Soylu <furkansylu97@gmail.com>
2026-02-07 02:23:32 +0300

    feat(intellectual-disability-page): add responsive design and layout optimizations

--- OzelFidanlar/src/features/home/pages/intellectual-disability-page/IntellectualDisabilityPage.tsx
+++ OzelFidanlar/src/features/home/pages/intellectual-disability-page/IntellectualDisabilityPage.tsx
@@ -4,7 +4,7 @@ const IntellectualDisabilityPage = () => {
   return (
     <>
       <IntellectualDisabilitySection />
-      <section aria-hidden className="h-24" />
+      <section aria-hidden className="h-10 lg:h-24" />
     </>
   );
 };
