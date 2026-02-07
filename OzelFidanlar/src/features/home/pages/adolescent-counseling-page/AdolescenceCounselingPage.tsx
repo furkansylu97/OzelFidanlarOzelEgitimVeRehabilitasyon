8b6cf211e3a85dda3a96f03588b2524950f09162
Furkan Soylu <furkansylu97@gmail.com>
2026-02-07 16:34:22 +0300

    feat(adolescent-counseling-page): add responsive design and layout optimizations

--- OzelFidanlar/src/features/home/pages/adolescent-counseling-page/AdolescenceCounselingPage.tsx
+++ OzelFidanlar/src/features/home/pages/adolescent-counseling-page/AdolescenceCounselingPage.tsx
@@ -4,7 +4,7 @@ const AdolescenceCounselingPage = () => {
   return (
     <>
       <AdolescenceCounselingSection />
-      <section aria-hidden className="h-24" />
+      <section aria-hidden className="h-0 lg:h-24" />
     </>
   );
 };
