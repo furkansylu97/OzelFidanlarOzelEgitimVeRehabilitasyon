a8e1a5f98f9851550f329c225ea6c0fe0f63a95a
Furkan Soylu <furkansylu97@gmail.com>
2026-02-07 17:22:29 +0300

    feat(guidance-psychological-counseling-page): add responsive design and layout optimizations

--- OzelFidanlar/src/features/home/pages/guidance-psychological-counseling-page/GuidancePsychologicalCounselingPage.tsx
+++ OzelFidanlar/src/features/home/pages/guidance-psychological-counseling-page/GuidancePsychologicalCounselingPage.tsx
@@ -4,7 +4,7 @@ const GuidancePsychologicalCounselingPage = () => {
   return (
     <>
       <GuidancePsychologicalCounselingSection />
-      <section aria-hidden className="h-24" />
+      <section aria-hidden className="h-0 lg:h-24" />
     </>
   );
 };
